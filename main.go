74625a6f2adc566ceaeb6b8c32b434dc201c9560
Siratee K. <sirateek@gmail.com>
2026-03-14 13:46:42 +0700

    fix: Updatet provider

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
+	"github.com/sirateek/terraform-provider-milvus/internal/provider"
 )
 
 var (
