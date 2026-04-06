28dacd2556eb9edbb9fc64b493d448444eb610f7
Siratee K. <sirateek@gmail.com>
2026-04-06 09:20:47 +0700

    fix: Increase timeout

--- internal/client/milvus/client.go
+++ internal/client/milvus/client.go
@@ -14,7 +14,7 @@ import (
 )
 
 func ProvideMilvusClient(config config.Milvus) (*milvusclient.Client, diag.Diagnostic) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	// Extract string values from pointers, using empty string as default
